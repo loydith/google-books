b26cd8edeb0c01f877a1736c4493d885b3740497
Loydith Valdivia <loydith@gmail.com>
2019-07-26 17:16:27 -0500

    create the functionality to search

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,86 @@ import API from "./utils/API";
 
 // import logo from "./logo.svg";
 
+import { Container, Row, Col } from "./components/Grid";
+
 class App extends Component {
+  state = {
+    books: [],
+    bookdsSearch: ""
+  };
+
+  handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get books update the books state
+    event.preventDefault();
+    API.getBooks(this.state.bookSearch)
+      .then(res => this.setState({ books: res.data }))
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          {/* <img className="App-logo"/> */}
-          <h2>(React) Google Books Search</h2>
-          <h3>Search for and Save Books of Interest</h3>
-        </div>
-        
+      <div>
+        <Nav />
+        <Jumbotron />
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <form>
+                <Container>
+                  <Row>
+                    <Col size="xs-9 sm-10">
+                      <Input
+                        name="bookSearch"
+                        value={this.state.bookSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Search for a title of book"
+                      />
+                    </Col>
+                    <Col size="xs-3 sm-2">
+                      <Button
+                        onClick={this.handleFormSubmit}
+                        type="success"
+                        className="input-lg"
+                      >
+                        Search
+                      </Button>
+                    </Col>
+                  </Row>
+                </Container>
+              </form>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="xs-12">
+              {!this.state.books.length ? (
+                <h1 className="text-center">No Book List to Display</h1>
+              ) : (
+                <BookList>
+                  {this.state.books.map(book => {
+                    return (
+                      <BookListItem
+                        key={book.title}
+                        title={book.title}
+                        href={book.href}
+                        ingredients={book.ingredients}
+                        thumbnail={book.thumbnail}
+                      />
+                    );
+                  })}
+                </BookList>
+              )}
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
